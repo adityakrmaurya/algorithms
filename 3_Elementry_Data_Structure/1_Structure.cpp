6ced536eb0b369db43766a78d4e66d616768d722
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-07-27 01:36:30 +0530

    feat: Added collinear function

--- 3_Elementry_Data_Structure/1_Structure.cpp
+++ 3_Elementry_Data_Structure/1_Structure.cpp
@@ -1,28 +1,32 @@
 // Structure is an aggregate type to define collection of data
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 // Point Data Interface
-struct Point{
+struct Point {
   float x, y;
 };
 // Calculates distance between the points
 float distance(Point, Point);
 // Convertes the point in polar form
-void polar(float, float, float*, float*);
+void polar(float, float, float *, float *);
+// checks if points are collinear
+bool collinear(Point, Point, Point);
 
-int main() 
-{
-
-}
+int main() {}
 
 float distance(Point a, Point b) {
   float dx = a.x - b.x;
   float dy = a.y - b.y;
-  return sqrt(dx*dx + dy*dy);
+  return sqrt(dx * dx + dy * dy);
 }
 
-void polar(float x, float y, float* r, float* theta) {
-  *r = sqrt(x*x + y*y);
+void polar(float x, float y, float *r, float *theta) {
+  *r = sqrt(x * x + y * y);
   *theta = atan2(x, y);
+}
+
+// using slope method
+bool collinear(Point a, Point b, Point c) {
+  return ((a.y - b.y) / (a.x - b.x)) == ((b.y - c.y) / (b.x - c.x));
 }
