aa94ced0f5f3a998a953a8a086f8cdc56549a311
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-08-11 20:17:55 +0530

    feat: Basics Compleated

---
+++ 9_Priority_Queues_And_Heap_Sort/2_Heaps.cpp
@@ -0,0 +1,12 @@
+/**
+ * In a heap, the records are stored in an array such that each key is
+ * guaranteed to be larger than the keys at two other specific positions
+ * -----
+ * A tree is heap-ordered if the key in each node is larger than or equal to
+ * the keys in all of that node's children
+ * -----
+ * A heap is a set of nodes with keys arranged in a complete heap-ordered
+ * binary tree, represented as an array
+ *
+ **/
+#include <iostream>
