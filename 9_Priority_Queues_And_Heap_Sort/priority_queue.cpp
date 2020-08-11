92fe560a27b3f508b9fe93f99eb76c5c2a2279fe
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-08-11 19:57:56 +0530

    docs: Added comments

--- 9_Priority_Queues_And_Heap_Sort/priority_queue.cpp
+++ 9_Priority_Queues_And_Heap_Sort/priority_queue.cpp
@@ -1,10 +1,13 @@
-/* 
+/**
+ * A priority queue is a data structure of items with keys that supports two
+ * basic operations: insert a new item, and remove the item with the largest key
+ * -----
  * Unordered Array Representation
  * Time Complexity:
  * insert: O(1)
- * remove maximum: O(N) 
+ * remove maximum: O(N)
  * remove: O(1)
- * find maximum: O(N) 
+ * find maximum: O(N)
  * change priority: 1
  * join: O(N)
  *
@@ -16,18 +19,27 @@ private:
   int size;
 
 public:
+  // constructs a queue of given capacity
   PQ(int capacity) {
     pq = new T[capacity];
     size = 0;
   }
+
+  // checks if queue is empty
   bool empty() const { return size == 0; }
+
+  // insert an element in the queue
   void insert(T item) { pq[size++] = item; }
+
+  // removes the maximum element from the queue
   T get_max() {
     int max_index = 0;
+    // finds the index of maximum element in the queue
     for (int i = 0; i < size; i++) {
       if (pq[i] > pq[max_index])
         max_index = i;
     }
+    //
     T temp = pq[max_index];
     pq[max_index] = pq[size - 1];
     pq[size - 1] = temp;
@@ -66,4 +78,5 @@ int main() {
   std::cout << pq.get_max();
   std::cout << pq.get_max();
   // ETSRTSUTONIIFB
+  return 0;
 }
