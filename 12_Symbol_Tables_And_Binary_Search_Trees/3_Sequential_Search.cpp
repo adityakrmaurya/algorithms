7e00959170cf060ee9b2355afc4fe9fe39063d79
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-08-20 20:02:33 +0530

    feat: ST using linked list

--- 12_Symbol_Tables_And_Binary_Search_Trees/3_Sequential_Search.cpp
+++ 12_Symbol_Tables_And_Binary_Search_Trees/3_Sequential_Search.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 using namespace std;
 
+// for Item Class see 1_Symbol-Table
 // array-based symbol table (ordered)
-template <typename Item, typename key> class ST {
+template <typename Item, typename Key> class STA {
 private:
   Item null_item, *st;
   int size;
 
 public:
-  ST(int capacity) {
+  STA(int capacity) {
     st = new Item[capacity + 1];
     size = 0;
   }
-
   int count() { return size; }
-
   void insert(Item x) {
     int i = size++;
     Key v = x.key();
@@ -24,23 +23,57 @@ public:
       st[i] = x;
     }
   }
-
   Item search(Key v) {
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < size; i++) {
       if (!(st[i].Key() < v))
         break;
+      if (v == st[i].key())
+        return st[i];
     }
-    if (v == st[i].key())
-      return st[i];
     return null_item;
   }
-
   Item select(int k) { return st[k]; }
   void show(ostream &os) {
     int i = 0;
-    while (i < N) {
+    while (i < size) {
       st[i++].show(os);
     }
+  }
+};
+
+// Linked List Based Symbol Tables
+template <typename Item, typename Key> class STLL {
+private:
+  Item null_item;
+  struct node {
+    Item item;
+    node *next;
+    node(Item x, node *t) {
+      item = x;
+      next = t;
+    }
+  };
+  typedef node *link;
+  int N;
+  link head;
+  Item searchR(link t, Key v) {
+    if (t == 0)
+      return null_item;
+    if (t->item.key() == v)
+      return t->item;
+    return searchR(t->next, v);
+  }
+
+public:
+  STLL(int maxN) {
+    head = 0;
+    N = 0;
+  }
+  int count() { return N; }
+  Item search(Key v) { return searchR(head, v); }
+  void insert(Item x) {
+    head = new node(x, head);
+    N++;
   }
 };
 int main(void) { return 0; }
